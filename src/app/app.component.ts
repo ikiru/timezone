fe8ab90d148c21c3cf05175317dd7ef1de727857
Jeff Winkler <jeff.a.winkler@gmail.com>
2017-07-17 13:30:01 -0500

    added time

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
-  title = 'app';
+  title = "US Time Zone Display";
+
+}
+
+function datetime()
+var time = new Date();
+if (){
+  this.time = time.setHours()
+} else if (){
+  this.time = time.setHours()+2
+  }else if (){
+  this.time =  time.setHours() +1
+  }else if (){
+  this.time =  time.setHours()-1
+}
 }
